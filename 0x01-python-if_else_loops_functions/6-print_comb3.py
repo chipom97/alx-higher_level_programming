bdc61728e6c6a82e204e40e86dd40576412d1fc9
Chipo Mazhani <chipomazhani@gmail.com>
2024-03-12 15:01:09 -0700

    6-print_comb3.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for a in range(0, 9):
+    for b in range(a+1, 10):
+        print("{}{}".format(a, b), end=", ")
+        if a == 8 and b == 9:
+            print("{}{}".format(a, b))
