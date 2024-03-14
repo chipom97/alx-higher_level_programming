1544f1f36249d748dad1cf2dfc1cc8d4099ec92f
Chipo Mazhani <chipomazhani@gmail.com>
2024-03-14 01:44:45 -0700

    Task_2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,22 +1,14 @@
-"""
-1. Write a program that prints the number of and the list of its arguments.
-
-The output should be:
-2. Number of argument(s) followed by argument (if number is one) or arguments (otherwise), followed by
-: (or . if no arguments were passed) followed by
-
-3. a new line, followed by (if at least one argument),
-
-4.one line per argument:
-
-5. the position of the argument (starting at 1) followed by :, followed by the argument value and a new line
-
-6. Your code should not be executed when imported
-
-7.The number of elements of argv can be retrieved by using: len(argv)
-
-8. You do not have to fully understand lists yet, but imagine that argv can be used just like a C array: you can use an index to walk through it. There are other ways (which will be preferred for future project tasks), if you know them you can use them.
-"""
-
 #!/usr/bin/python3
+if __name__ == "__main__":
 
+    import sys
+    count = len(sys.argv) - 1
+
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
