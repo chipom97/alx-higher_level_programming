60ab665b924ce551f0202cfc92fbf713490cfbc0
Chipo Mazhani <chipomazhani@gmail.com>
2024-04-05 09:27:40 -0700

    white space

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,14 +6,15 @@ class Square:
     """Represent a square."""
 
     def __init__(self, size=0):
-        """ A special method that is called when an instance of the class is called"""
-        
+        """ A special method that is called 
+when an instance of the class is called"""
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        
+
     def area(self):
         """ Public instance that returns the area. """
 
