5370940c0e043210d46f4cb6278e28244c4b69dc
Chipo Mazhani <chipomazhani@gmail.com>
2024-07-08 13:08:03 +0000

    task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    """function that prints a matrix of integers."""
+    for inner in matrix:
+        for num in inner:
+            if num == inner[(len(inner) - 1)]:
+                print("{}".format(num), end="")
+            else:
+                print("{}".format(num), end =" ")
+        print()
