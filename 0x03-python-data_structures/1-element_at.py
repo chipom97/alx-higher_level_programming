79149d265b04dfc0884af78a85c0b0f8384e8fe0
Chipo Mazhani <chipomazhani@gmail.com>
2024-07-08 10:32:36 +0000

    edited task_1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@
 def element_at(my_list, idx):
     """ function that retrieves an element from a list like """
     list_len = len(my_list)
-    if idx < 0 or idx > (len(list_len) -1):
+    if idx < 0 or idx > (list_len -1):
         return none
     return my_list[idx]
