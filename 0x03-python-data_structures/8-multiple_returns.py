4d3372648087ea77431572f6fe01cc4c09621ae2
Chipo Mazhani <chipomazhani@gmail.com>
2024-07-09 09:18:45 +0000

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,7 +4,6 @@ def multiple_returns(sentence):
     """function that returns a tuple with the
 length of a string and its first character."""
 
-    length = 0
-    for i in range(len(sentence)):
-        length += 1
-    return length, sentence[0]
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
