46c2417007b8d641b2939944bf50a7e46b54e538
Chipo Mazhani <chipomazhani@gmail.com>
2024-07-11 11:45:14 +0000

    task 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """function that returns the number of keys in a dictionary"""
+    count = 0
+    for key in a_dictionary:
+        count += 1
+    return count
