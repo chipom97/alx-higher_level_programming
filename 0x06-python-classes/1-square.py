1eee3fc5b2c6de1c9025a465831632db6fa9b028
Chipo Mazhani <chipomazhani@gmail.com>
2024-07-24 09:57:24 +0000

    Task_!

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,12 @@
-"""Define a class Square.""""
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+        Take size (private attribute) as first argument
+        """
+        self.__size = size
