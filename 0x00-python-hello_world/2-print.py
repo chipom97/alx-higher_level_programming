e4b8158d7c80947395193d872323af7394102150
Chipo Mazhani <chipomazhani@gmail.com>
2025-02-22 13:37:14 +0200

    2-print.py

---
+++ 0x00-python-hello_world/2-print.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+Write a Python script that prints exactly
+"Programming is like building a multilingual puzzle,
+followed by a new line.
+
+Use the function print
+"""
+
+print("\"Programming is like building a multilingual puzzle")
