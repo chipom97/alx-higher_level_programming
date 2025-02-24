d1d5df1ba3a1ffbb68964f4e53daae284b67def8
Chipo Mazhani <chipomazhani@gmail.com>
2025-02-24 21:43:37 +0200

    Task_1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,16 +4,9 @@ number = random.randint(-10000, 10000)
 
 last_digit = abs(number) % 10
 
-"""
-The output of the program should be:
-The string Last digit of, followed by
-the number, followed by
-the string is, followed by the last digit of number, followed by
-if the last digit is greater than 5: the string and is greater than 5
-if the last digit is 0: the string and is 0
-if the last digit is less than 6 and not 0: the string and is less than 6 and not 0
-followed by a new line
-"""
+if number < 0:
+    last_digit = -(last_digit)
+
 print(f"Last digit of {number} is {last_digit} and is", end=" ")
 
 if last_digit > 5:
