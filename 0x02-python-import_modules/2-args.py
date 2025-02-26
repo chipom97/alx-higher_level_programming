d4a86d78e6b81a9a10c556f7fb16d2637e7a9c55
Chipo Mazhani <chipomazhani@gmail.com>
2025-02-27 01:14:43 +0200

    correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         print("0 arguments.")
 
     elif length == 1:
-        print("1 argument")
+        print("1 argument:")
         print("1: {}".format(argv[1]))
 
     else:
