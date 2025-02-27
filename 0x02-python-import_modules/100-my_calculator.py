b4587b4790c9b11e499d60eac434108dd9badc87
Chipo Mazhani <chipomazhani@gmail.com>
2025-02-27 12:16:01 +0200

    Task_6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div
+import sys
+
+length = len(sys.argv) - 1
+operators = ["*", "-", "/", "+"]
+
+
+def operator():
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if sys.argv[2] == "+":
+        print("{} + {} = {}".format(a, b, add(a, b)))
+        sys.exit(0)
+    elif sys.argv[2] == "-":
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+        sys.exit(0)
+    elif sys.argv[2] == "*":
+        print(sys.argv[2])
+        # print("{} * {} = {}".format(a, b, mul(a, b)))
+        sys.exit(0)
+    elif sys.argv[2] == "/":
+        print("{} / {} = {}".format(a, b, div(a, b)))
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    if length < 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    elif length == 3:
+        op = sys.argv[2]
+        if op in operators:
+            operator()
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
---
+++ 0x02-python-import_modules/mul.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+from calculator_1 import mul
+
+print(mul(5, 5))
