d7023568245fa321360e5759984c8304ea261b00
Chipo Mazhani <chipomazhani@gmail.com>
2025-10-16 11:06:18 +0200

    spaces

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-	"""Returns the length of a string and its first character."""
-	if sentence == "":
-		return (0, None)
+    """Returns the length of a string and its first character."""
+    if sentence == "":
+        return (0, None)
 
-	length = len(sentence)
-	first = sentence[0]
+    length = len(sentence)
+    first = sentence[0]
 
-	return (length, first)
+    return (length, first)
